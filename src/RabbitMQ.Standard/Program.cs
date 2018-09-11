c46e9c360ec805da69a0c31eb865a71411d4b5f6
Bruno Arruda <arruda@outlook.com>
2018-09-11 15:22:26 +0200

    Add standard project and common

---
+++ src/RabbitMQ.Standard/Program.cs
@@ -0,0 +1,86 @@
+﻿using Common;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+
+namespace RabbitMQ.Santard
+{
+    internal class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string QueueName = "StandardQueue_ExampleQueue";
+
+        private static void Main(string[] args)
+        {
+            Payment payment1 = new Payment { AmountToPay = 25.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment2 = new Payment { AmountToPay = 5.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment3 = new Payment { AmountToPay = 2.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment4 = new Payment { AmountToPay = 17.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment5 = new Payment { AmountToPay = 300.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment6 = new Payment { AmountToPay = 350.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment7 = new Payment { AmountToPay = 295.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment8 = new Payment { AmountToPay = 5625.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment9 = new Payment { AmountToPay = 5.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+            Payment payment10 = new Payment { AmountToPay = 12.0m, CardNumber = "1234123412341234", Name = "Mr S Haunts" };
+
+            CreateQueue();
+
+            SendMessage(payment1);
+            SendMessage(payment2);
+            SendMessage(payment3);
+            SendMessage(payment4);
+            SendMessage(payment5);
+            SendMessage(payment6);
+            SendMessage(payment7);
+            SendMessage(payment8);
+            SendMessage(payment9);
+            SendMessage(payment10);
+
+            Receive();
+
+            Console.ReadLine();
+
+        }
+
+        private static void CreateQueue()
+        {
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static void SendMessage(Payment message)
+        {
+            _model.BasicPublish("", QueueName, null, message.Serialize());
+            Console.WriteLine(" [x] Payment Message Sent : {0} : {1} : {2}", message.CardNumber, message.AmountToPay, message.Name);
+        }
+
+        public static void Receive()
+        {
+            EventingBasicConsumer consumer = new EventingBasicConsumer(_model);
+            uint msgCount = GetMessageCount(_model, QueueName);
+
+            _model.BasicConsume(QueueName, true, consumer);
+
+
+            consumer.Received += (model, ea) =>
+            {
+                Payment message = (Payment)ea.Body.DeSerialize(typeof(Payment));
+
+                Console.WriteLine("----- Received {0} : {1} : {2}", message.CardNumber, message.AmountToPay, message.Name);
+            };
+
+        }
+
+        private static uint GetMessageCount(IModel channel, string queueName)
+        {
+            QueueDeclareOk results = channel.QueueDeclare(queueName, true, false, false, null);
+            return results.MessageCount;
+        }
+    }
+}
